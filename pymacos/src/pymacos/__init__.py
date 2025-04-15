c08c35c7bcfa586e1617e123130eb7eb34d75610
TraceMoreRays <sigrist1000@gmail.com>
2025-04-14 22:34:08 -0700

    Defining the location must come before importing the MACOS API.

--- pymacos/src/pymacos/__init__.py
+++ pymacos/src/pymacos/__init__.py
@@ -8,17 +8,18 @@ A module for analyzing / designing optical systems.
 """
 from __future__ import absolute_import
 
-from .macos import *
-from .version import __version__
-
 # -------------------
 # needed when non-anaconda Python version is used; otherwise, pymacos DLL
 # cannot be loaded (missing Intel & MS DLL's)
 #  => requires Intel Redistributable to be installed (or add to search path?)
+#  => must be placed before importing the macos API
 #
 # import os
 # os.add_dll_directory("C:\\Program Files (x86)\\Intel\\oneAPI\\2025.0\\bin")
 # -------------------
+
+from .macos import *
+from .version import __version__
 
 # Note: Python's MACOS DLL (pymacosf90.cp313-win_amd64.pyd) is to placed in
 #       this folder.
