6ff1e8ac76f800b9faa1b7834500f232ee989514
TraceMoreRays <sigrist1000@gmail.com>
2025-09-18 16:28:16 -0700

    upd: if loading pymacos fails, DLL path is set and re-tried. User must enter the correct path to the oneAPI DLL location. (Only on Windows)

--- pymacos/src/pymacos/__init__.py
+++ pymacos/src/pymacos/__init__.py
@@ -14,12 +14,25 @@ from __future__ import absolute_import
 #  => requires Intel Redistributable to be installed (or add to search path?)
 #  => must be placed before importing the macos API
 #
-# import os
-# os.add_dll_directory("C:\\Program Files (x86)\\Intel\\oneAPI\\2025.2\\bin")
-# -------------------
 
-from .macos import *
-from .version import __version__
+try: 
+    from .macos import *
+    from .version import __version__
+
+except (ImportError):
+
+    # ONLY on Windows  (can be ignored with Anaconda Environments)
+    dll_path = "C:\\Program Files (x86)\\Intel\\oneAPI\\2025.2\\bin"
+
+    import os
+    if hasattr(os, 'add_dll_directory') and dll_path.strip():
+        if not os.path.exists(dll_path):
+            raise FileExistsError(f"DLL Path {dll_path} not found")
+        os.add_dll_directory(dll_path)
+    # -------------------
+    from .macos import *
+    from .version import __version__
+
 
 # Note: Python's MACOS DLL (pymacosf90.cp313-win_amd64.pyd) is 
 #       to be placed in this folder.
